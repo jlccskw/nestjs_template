b9791cd9a80959f418facfe6354f8ec00e16a8d0
Kevin <114189868@qq.com>
2023-11-30 08:30:53 +0800

    20231130早提交

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    LoggerModule,
   ],
 })
 export class AppModule {}
